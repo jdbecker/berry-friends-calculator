d8edbfab573736f52207525cd7f5360003830b0b
jdbecker <jdbecker@users.noreply.github.com>
2023-07-25 16:39:37 +0000

    seems to be working!

--- berry_calculator.py
+++ berry_calculator.py
@@ -1,26 +1,28 @@
-def input_int(prompt: string, default: int) -> int:
+def input_int(prompt: str, default: int = 0) -> int:
     response = input(prompt)
-    if default and response == '':
+    if default and response == "":
         return default
-    elif isinstance(response, int):
-        return response
-    else:
-        print('Response must be an int!')
+
+    try:
+        return int(response)
+    except ValueError:
+        print("Response must be an int!")
         return input_int(prompt, default)
-    
-def input_bool(prompt: string, default: bool) -> bool:
+
+
+def input_bool(prompt: str, default: bool) -> bool:
     response = input(prompt)
-    if default and response == '':
+    if default and response == "":
         return default
-    elif isinstance(response, bool):
-        return response
-    elif isinstance(response, str):
-        if response.lower() in ['y', 'yes']:
-            return True
-        elif response.lower() in ['n', 'no']:
-            return False
+
+    if response.lower() in ["y", "yes"]:
+        return True
+    if response.lower() in ["n", "no"]:
+        return False
+
     print("Response must be 'yes' or 'no'")
     return input_bool(prompt, default)
+
 
 def give_berry(current_friendship: int, soothe_bell: bool) -> int:
     if current_friendship < 100:
@@ -30,19 +32,21 @@ def give_berry(current_friendship: int, soothe_bell: bool) -> int:
     else:
         increase_by = 2
     if soothe_bell:
-        increase_by = increase_by + increase_by / 2
+        increase_by = increase_by + int(increase_by / 2)
     return current_friendship + increase_by
 
 
-if __name__ == '__main__':
-    starting_friendship = input_int('Current friendship: ')
-    target_friendship = input_int('Target Friendship (default 220): ', 220)
-    soothe_bell = input_bool('Using soothe bell? (Y/n): ', True)
-    
-    berries = 0
+if __name__ == "__main__":
+    starting_friendship: int = input_int("Current friendship: ")
+    target_friendship = input_int("Target Friendship (default 220): ", 220)
+    soothe_bell = input_bool("Using soothe bell? (Y/n): ", True)
+
+    berries: int = 0
     current_friendship = starting_friendship
     while current_friendship < target_friendship:
         current_friendship = give_berry(current_friendship, soothe_bell)
         berries += 1
-    
-    print(f"It takes {berries} friendship berries to get your pokemon from {starting_friendship} to {current_friendship}")
+
+    print(
+        f"It takes {berries} friendship berries to get your pokemon from {starting_friendship} to {current_friendship}"
+    )
